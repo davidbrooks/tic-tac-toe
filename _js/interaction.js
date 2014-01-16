0a6e405bccbcdeb2ff5676e53e1e6ad39a707ee1
David <david@davidnbrooks.com>
2014-01-16 17:58:57 -0500

    This adds the preliminary .js and .sass files

---
+++ _js/interaction.js
@@ -0,0 +1,12 @@
+var t = (function($, window, document, undefined) {
+	$('document').ready(function(){
+		t.init();
+	});
+	return {
+		init: function(){
+			t.clicks();
+		},
+		clicks: function(){
+		}
+	};
+})(jQuery, this, this.document);
